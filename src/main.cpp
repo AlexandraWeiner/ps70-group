96a1c69573672d297dc3357e7e806d8c3185408a
AlexandraWeiner <alexandra.weiner1@gmail.com>
2023-04-05 22:35:53 -0400

    working write to firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,272 @@
-// /*
-//   Rui Santos
-//   Complete project details at our blog.
-//     - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-//     - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-//   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-//   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//   Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-//   https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-// */
-
-// #include <Arduino.h>
-// #if defined(ESP32)
-//   #include <WiFi.h>
-// #elif defined(ESP8266)
-// #endif
-// #include <Firebase_ESP_Client.h>
-
-// //Provide the token generation process info.
-// #include "addons/TokenHelper.h"
-// //Provide the RTDB payload printing info and other helper functions.
-// #include "addons/RTDBHelper.h"
-
-// // Insert your network credentials
-// #define WIFI_SSID ""
-// #define WIFI_PASSWORD ""
-
-// // Insert Firebase project API Key
-// #define API_KEY "AIzaSyCtkC6xAis3Gw6N_ADcXDwp6OIQ-i1JkzM"
-
-// // Insert RTDB URLefine the RTDB URL */
-// #define DATABASE_URL "https://ps70-d90a6-default-rtdb.firebaseio.com/" 
-
-// //Define Firebase Data object
-// FirebaseData fbdo;
-
-// FirebaseAuth auth;
-// FirebaseConfig config;
-
-// unsigned long sendDataPrevMillis = 0;
-// int count = 0;
-// bool signupOK = false;
-
-// void setup(){
-//   Serial.begin(115200);
-//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-//   Serial.print("Connecting to Wi-Fi");
-//   while (WiFi.status() != WL_CONNECTED){
-//     Serial.print(".");
-//     delay(300);
-//   }
-//   Serial.println();
-//   Serial.print("Connected with IP: ");
-//   Serial.println(WiFi.localIP());
-//   Serial.println();
-
-//   /* Assign the api key (required) */
-//   config.api_key = API_KEY;
-
-//   /* Assign the RTDB URL (required) */
-//   config.database_url = DATABASE_URL;
-
-//   /* Sign up */
-//   if (Firebase.signUp(&config, &auth, "", "")){
-//     Serial.println("ok");
-//     signupOK = true;
-//   }
-//   else{
-//     Serial.printf("%s\n", config.signer.signupError.message.c_str());
-//   }
-
-//   /* Assign the callback function for the long running token generation task */
-//   config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  
-//   Firebase.begin(&config, &auth);
-//   Firebase.reconnectWiFi(true);
-// }
-
-// void loop(){
-//   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
-//     sendDataPrevMillis = millis();
-//     // Write an Int number on the database path test/int
-//     if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + fbdo.dataPath());
-//       Serial.println("TYPE: " + fbdo.dataType());
-//     }
-//     else {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + fbdo.errorReason());
-//     }
-//     count++;
-    
-//     // Write an Float number on the database path test/float
-//     if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + fbdo.dataPath());
-//       Serial.println("TYPE: " + fbdo.dataType());
-//     }
-//     else {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + fbdo.errorReason());
-//     }
-//   }
-// }
-
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
-*********/
-
+#include <FastLED.h>
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#endif
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
-#define LED 18
-Adafruit_NeoPixel pixels(1, LED, NEO_GRB + NEO_KHZ800);
+#define LED_PIN     19
+#define NUM_LEDS    10
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define API_KEY "AIzaSyCtkC6xAis3Gw6N_ADcXDwp6OIQ-i1JkzM"
+#define DATABASE_URL "https://ps70-d90a6-default-rtdb.firebaseio.com"
+
+
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
+// This example shows several ways to set up and use 'palettes' of colors
+// with FastLED.
+//
+// These compact palettes provide an easy way to re-colorize your
+// animation on the fly, quickly, easily, and with low overhead.
+//
+// USING palettes is MUCH simpler in practice than in theory, so first just
+// run this sketch, and watch the pretty lights as you then read through
+// the code.  Although this sketch has eight (or more) different color schemes,
+// the entire sketch compiles down to about 6.5K on AVR.
+//
+// FastLED provides a few pre-configured color palettes, and makes it
+// extremely easy to make up your own color schemes with palettes.
+//
+// Some notes on the more abstract 'theory and practice' of
+// FastLED compact palettes are at the bottom of this file.
+
+
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+const char* ssid = "iPhone (188)";
+const char* password =  "thereitis";
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pixels.begin(); // INITIALIZE NeoPixel (REQUIRED)
-  //pinMode(LED, OUTPUT);
+    delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the RTDB URL (required) */
+    config.database_url = DATABASE_URL;
+
+    /* Sign up */
+    if (Firebase.signUp(&config, &auth, "", "")){
+      Serial.println("Firebase ok");
+      signupOK = true;
+    }
+    else{
+      Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    }
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+    
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+    
+    currentPalette = RainbowColors_p;
+    currentBlending = LINEARBLEND;
 }
 
-// Simple function to return a color in the rainbow
-// Input a value 0 to 255 to get a color value.
-uint32_t Wheel(byte WheelPos)
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    //Assume the wheel value is less than 85, if so Green value is 0
-    uint32_t returnColor = Adafruit_NeoPixel::Color((byte)(255 - (WheelPos * 3)), 0, (byte)(WheelPos * 3));
-
-    //If we are greater than 170 Red value is 0
-    if (WheelPos > 84 && WheelPos < 170)
-    {
-        WheelPos -= 85;
-        returnColor = Adafruit_NeoPixel::Color(0, (byte)(WheelPos * 3), (byte)(255 - WheelPos * 3));
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
     }
-    //Finally above 170 and Blue value is 0
-    else if (WheelPos >= 170)
-    {
-        WheelPos -= 170;
-        returnColor = Adafruit_NeoPixel::Color((byte)(WheelPos * 3), (byte)(255 - WheelPos * 3), 0);
-    }
-
-    return returnColor;
 }
 
-int colorCount = 0;
-void loop() {
-  // put your main code here, to run repeatedly:
-  pixels.setPixelColor(0, Wheel(colorCount++));
-  Serial.println("LED is on");
-  // Send the updated pixel colors to the hardware.
-  pixels.show();  
-  delay(1000);
-  pixels.clear(); // Set pixel colors to 'off'
-  Serial.println("LED is off");
-  delay(1000);
-  //Cycle the colors at the end.
-  if (colorCount > 255)
-      colorCount = 0;
+
+// There are several different palettes of colors demonstrated here.
+//
+// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
+// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+//
+// Additionally, you can manually define your own color palettes, or you can write
+// code that creates color palettes on the fly.  All are shown here.
+
+
+
+// This function fills the palette with totally random colors.
+void SetupTotallyRandomPalette()
+{
+    for( int i = 0; i < 16; ++i) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
 }
+
+// This function sets up a palette of black and white stripes,
+// using code.  Since the palette is effectively an array of
+// sixteen CRGB colors, the various fill_* functions can be used
+// to set them up.
+void SetupBlackAndWhiteStripedPalette()
+{
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::White;
+    currentPalette[4] = CRGB::White;
+    currentPalette[8] = CRGB::White;
+    currentPalette[12] = CRGB::White;
+    
+}
+
+// This function sets up a palette of purple and green stripes.
+void SetupPurpleAndGreenPalette()
+{
+    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+    CRGB green  = CHSV( HUE_GREEN, 255, 255);
+    CRGB black  = CRGB::Black;
+    
+    currentPalette = CRGBPalette16(
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black,
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black );
+}
+
+
+// This example shows how to set up a static color palette
+// which is stored in PROGMEM (flash), which is almost always more
+// plentiful than RAM.  A static PROGMEM palette like this
+// takes up 64 bytes of flash.
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Gray, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
+void ChangePalettePeriodically()
+{
+    uint8_t secondHand = (millis() / 1000) % 60;
+    static uint8_t lastSecond = 99;
+    
+    if( lastSecond != secondHand) {
+        lastSecond = secondHand;
+        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
+        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
+        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
+        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
+        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
+        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
+        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
+        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
+        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
+    }
+}
+
+void loop()
+{
+
+    ChangePalettePeriodically();
+    
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    
+    FillLEDsFromPaletteColors( startIndex);
+    
+    FastLED.show();
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+
+    if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+      // Write an Int number on the database path test/int
+      if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+        Serial.println("PASSED; dataPath; dataType");
+        Serial.println(fbdo.dataPath());
+        Serial.println(fbdo.dataType());
+      }
+      else {
+        Serial.println("FAILED; errorReason");
+        Serial.println(fbdo.errorReason());
+      }
+      count++;
+      
+      // Write an Float number on the database path test/float
+      if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+        Serial.println("PASSED; path; type");
+        Serial.println(fbdo.dataPath());
+        Serial.println(fbdo.dataType());
+      }
+      else {
+        Serial.println("FAILED; errorReason");
+        Serial.println(fbdo.errorReason());
+      }
+  }
+
+}
+
+
+// Additional notes on FastLED compact palettes:
+//
+// Normally, in computer graphics, the palette (or "color lookup table")
+// has 256 entries, each containing a specific 24-bit RGB color.  You can then
+// index into the color palette using a simple 8-bit (one byte) value.
+// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
+// is quite possibly "too many" bytes.
+//
+// FastLED does offer traditional 256-element palettes, for setups that
+// can afford the 768-byte cost in RAM.
+//
+// However, FastLED also offers a compact alternative.  FastLED offers
+// palettes that store 16 distinct entries, but can be accessed AS IF
+// they actually have 256 entries; this is accomplished by interpolating
+// between the 16 explicit entries to create fifteen intermediate palette
+// entries between each pair.
+//
+// So for example, if you set the first two explicit entries of a compact 
+// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
+// the first sixteen entries from the virtual palette (of 256), you'd get
+// Green, followed by a smooth gradient from green-to-blue, and then Blue.
